24736e385cec47dfe52c9ec1ebb1482312c27495
Annick <annick.hauregard@poe.aelion.biz>
2023-03-16 16:45:49 +0100

    add crossOrigin

--- src/main/java/fr/aelion/streamer/controllers/CourseController.java
+++ src/main/java/fr/aelion/streamer/controllers/CourseController.java
@@ -4,6 +4,7 @@ import fr.aelion.streamer.dto.FullCourseDto;
 import fr.aelion.streamer.entities.Course;
 import fr.aelion.streamer.services.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class CourseController {
     @Autowired
     private CourseService service;
     @GetMapping // associer une method a un verb et a une uri
+    @CrossOrigin
     public List<FullCourseDto> findAll() {
        return service.findAll();
     }
